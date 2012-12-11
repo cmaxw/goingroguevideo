3383ef0d01ab5ed6014748f2e233224bf5d61607
Charles Max Wood <chuck@teachmetocode.com>
2012-12-11 14:31:57 -0700

    rails basics + database config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsrampup::Application.config.secret_token = '8906eeff1bbd0e5aef85cd785075888ad6079dd917255a2c692c8386f3cfb140ee40ce67befc5332108e09f99933e41af989256f70942a3f492af25232f5c6fa'
