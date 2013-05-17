a9cb26989a9026a9128add8861abbe0ab81c4a51
Charles Max Wood <chuck@teachmetocode.com>
2013-05-16 21:57:10 -0600

    fix the debugging video link

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Railsrampup::Application.routes.draw do
   get 'about', :to => "home#about"
   get 'thanks', :to => "home#thanks"
-  get 'debugging-video', :to => "home#debugging-video'
+  get 'debugging-video', :to => "home#debugging-video"
 
   root :to => 'home#index'
 
